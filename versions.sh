ae370fa48960947faffd606490fed40157e2a34a
Paweł 'felixd' Wojciechowski <felixd@wp.pl>
2016-01-30 00:05:20 +0100

    Update versions.sh

--- versions.sh
+++ versions.sh
@@ -5,7 +5,8 @@
 
 RUBY_VER="2.2.4"
 
-# Uncomment below line fi you need specific Ruby on Rails version.
-# Otherwise laster, stable Ruby on Rails will be installed.
+# Uncomment below line if you need specific Ruby on Rails version.
+# Otherwise latest, stable Ruby on Rails will be installed.
+# gem install rails
 
 #RUBY_ON_RAILS_VER="4.2.5.1"
