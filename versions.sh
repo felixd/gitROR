8eb5668a19f0d25aea403768e3f24b40f8ec404d
Paweł Wojciechowski <felixd@konopnickiej.com>
2016-02-05 12:24:21 +0100

    Ruby version change: 2.3.0

--- versions.sh
+++ versions.sh
@@ -3,7 +3,7 @@
 # © Outsourcing IT - Konopnickiej.Com
 # Author: Paweł Wojciechowski
 
-RUBY_VER="2.2.4"
+RUBY_VER="2.3.0"
 
 # Uncomment below line if you need specific Ruby on Rails version.
 # Otherwise latest, stable Ruby on Rails will be installed.
