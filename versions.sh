0043df9a54482667a61a55aeac6cccc7e4814241
Paweł Wojciechowski <felixd@konopnickiej.com>
2020-03-31 17:03:04 +0200

    2.5.3 Ruby ver

--- versions.sh
+++ versions.sh
@@ -3,7 +3,7 @@
 # © Outsourcing IT - Konopnickiej.Com
 # Author: Paweł Wojciechowski
 
-RUBY_VER="2.3.1"
+RUBY_VER="2.5.3"
 
 # Uncomment below line if you need specific Ruby on Rails version.
 # Otherwise latest, stable Ruby on Rails will be installed.
