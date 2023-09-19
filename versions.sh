032c6fd164604285813a66cd8576f552dd8eb609
Paweł Wojciechowski <felixd@konopnickiej.com>
2023-09-19 10:23:16 +0200

    Update versions.sh

--- versions.sh
+++ versions.sh
@@ -3,7 +3,10 @@
 # © Outsourcing IT - Konopnickiej.Com
 # Author: Paweł Wojciechowski
 
-RUBY_VER="2.5.3"
+RUBY_VER="2.7.8"
+
+#RUBY_VER="3.2.2"
+#RUBY_VER="3.3.0-dev"
 
 # Uncomment below line if you need specific Ruby on Rails version.
 # Otherwise latest, stable Ruby on Rails will be installed.
